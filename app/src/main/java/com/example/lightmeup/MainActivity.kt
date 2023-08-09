e760cf78ee257712e262a3896b02c775451eba94
harrySwin <140131681+harrySwin@users.noreply.github.com>
2023-08-09 15:43:13 +1000

    Update MainActivity.kt

--- app/src/main/java/com/example/lightmeup/MainActivity.kt
+++ app/src/main/java/com/example/lightmeup/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lightmeup
 
+import android.graphics.drawable.Drawable
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,22 +10,45 @@ import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var lockState = "closed"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val key : ImageButton = findViewById<ImageButton>(R.id.key_btn)
         val lock : ImageView = findViewById<ImageView>(R.id.lock)
-        lock.setTag("closed")
+
+        checkKey(lockState, lock)
+
+        if (savedInstanceState != null) {
+            lockState = savedInstanceState.getString("LOCKSTATE").toString()
+            checkKey(lockState, lock)
+        }
+
         key.setOnClickListener {
-            if (lock.getTag() != "open") {
-                lock.setImageResource(R.drawable.round_lock_open_black_48)
-                lock.setTag("open")
-            }
-            else {
-                lock.setImageResource(R.drawable.round_lock_black_48)
-                lock.setTag("closed")
-            }
+            changeLock(lock)
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("LOCKSTATE",lockState)
+    }
+    fun checkKey(lockState: String, lock: ImageView) {
+        if (lockState == "open") {
+            lock.setImageResource(R.drawable.round_lock_open_black_48)
+        }
+        else {
+            lock.setImageResource(R.drawable.round_lock_black_48)
+        }
+    }
+    fun changeLock(lock: ImageView) {
+        if (lockState != "open") {
+            lock.setImageResource(R.drawable.round_lock_open_black_48)
+            this.lockState = ("open")
+        }
+        else {
+            lock.setImageResource(R.drawable.round_lock_black_48)
+            this.lockState = ("closed")
         }
     }
 }
